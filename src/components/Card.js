c9b3c98900b2e0b89125350ebad877aaae4c5c94
Sergey Ladorski <sergeyladorski@gmail.com>
2022-01-25 13:18:58 +0300

    styles renewed desctop, styles added mobile, cards per page updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,13 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export const Card = (props) => {
     return (
         <Link className='card' to={`/photos/${props.id}`}>
             <img className='card__image' src={props.src} alt={props.alt} />
-            <p className='card__title'>{props.title}</p>
-            <p className='card__subtitle'>{props.subtitle}</p>
+            <div className='card__description'>
+                <p className='card__title'>{props.title}</p>
+                <p className='card__subtitle'>{props.subtitle}</p>
+            </div>
         </Link>
     )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,10 @@
 export function Footer() {
+    const currentYear = new Date().getFullYear();
+
     return (
         <footer className='footer'>
             <p className='footer__copyright'>
-                &copy; 2021 Sergey Ladorski
+                &copy; {currentYear} Mesto Russia
             </p>
         </footer>
     );
